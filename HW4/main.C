d9220bd97e6cb28c51b5ee1cf17fe2be67874104
Sayhello_bro <144075540+Sayhellobro@users.noreply.github.com>
2024-04-29 22:41:34 +0800

    Create main.C

---
+++ HW4/main.C
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class TicTacToe{
+    private:
+        
+        
+    public:
+        char board[10]={'1','2','3','4','5','6','7','8','9'};
+        void print_board(char Board[10]){
+            
+        }
+}
+
+int main()
+{
+    TicTacToe game;
+    game.run();
+    
+    return 0;
+}
